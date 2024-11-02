62f318ab7b5f0cfe4bafb56799da7f640fc83558
Lukas Turcani <lukasturcani93@gmail.com>
2024-11-02 17:48:51 +0000

    wip

--- src/aitomic/_internal/bruker.py
+++ src/aitomic/_internal/bruker.py
@@ -1,9 +1,18 @@
 import tempfile
+import zipfile
+from collections.abc import Iterator
+from dataclasses import dataclass
+from pathlib import Path
 
+import nmrglue
 import polars as pl
 
 
-def nmr_peaks_df_1d(zip_file: bytes) -> pl.DataFrame:
+def nmr_peaks_df_1d(
+    zip_file: bytes,
+    *,
+    peak_threshold: float = 1e4,
+) -> pl.DataFrame:
     """Exctact peaks from a zip of multiple Bruker NMR spectra.
 
     Examples:
@@ -11,9 +20,49 @@ def nmr_peaks_df_1d(zip_file: bytes) -> pl.DataFrame:
 
     Parameters:
         zip_file: The content zip file containing Bruker data.
+        peak_threshold: Minimum peak height for positive peaks.
 
     Returns:
         The peaks as a polars data frame.
 
     """
-    pass
+    with tempfile.TemporaryDirectory() as tmp:
+        with Path(tmp, "spectra.zip").open("wb") as f:
+            f.write(zip_file)
+        zipfile.ZipFile(tmp).extractall(tmp)
+        ppms = []
+        volumes = []
+        spectra = []
+        for spectrum_dir in Path(tmp).glob("*"):
+            for peak in _pick_peaks(spectrum_dir, peak_threshold):
+                ppms.append(peak.ppm)
+                volumes.append(peak.volume)
+                spectra.append(spectrum_dir.name)
+        return pl.DataFrame(
+            {
+                "spectrum": spectra,
+                "ppm": ppms,
+                "volume": volumes,
+            }
+        )
+
+
+@dataclass(slots=True, frozen=True)
+class NmrPeak:
+    ppm: float
+    volume: float
+
+
+def _pick_peaks(
+    spectrum_dir: Path,
+    peak_threshold: float,
+) -> Iterator[NmrPeak]:
+    metadata, data = nmrglue.bruker.read_pdata(str(spectrum_dir))
+    udic = nmrglue.bruker.guess_udic(metadata, data)
+    unit_conversion = nmrglue.fileio.fileiobase.uc_from_udic(udic)
+    for peak in nmrglue.peakpick.pick(
+        data, pthres=peak_threshold, nthres=None
+    ):
+        ppm = unit_conversion.ppm(peak["X_AXIS"])
+        volume = peak["VOL"]
+        yield NmrPeak(ppm, volume)
