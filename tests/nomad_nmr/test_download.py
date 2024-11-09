e0306c9f9679e0ce26bc64e3eb6c10406afc5384
Lukas Turcani <lukasturcani93@gmail.com>
2024-11-09 01:09:39 +0000

    wip

--- tests/nomad_nmr/test_download.py
+++ tests/nomad_nmr/test_download.py
@@ -18,24 +18,18 @@ def test_download_all() -> None:
 
         expected_files = sorted(
             [
-                "2106231050-2-1-test1-10.json",
-                "2106231050-2-1-test1-11.json",
-                "2106231055-3-2-test2-10.json",
-                "2106231100-10-2-test3-10.json",
-                "2106240012-10-2-test2-10.json",
-                "2106241100-10-2-test3-10.json",
-                "2106241100-10-2-test4-1.json",
+                "2409231309-0-2-lukasturcani-10",
+                "2409231309-0-3-lukasturcani-10",
+                "2410081201-0-1-lukasturcani-10",
+                "2410161546-0-1-admin-10",
+                "2410161546-0-1-admin-11",
             ]
         )
         assert len(experiments) == len(expected_files)
         with zipfile.ZipFile(f.name) as zip_file:
             files = sorted(zip_file.namelist())
+            print(files)
             assert files == expected_files
-            for file in files:
-                assert (
-                    zip_file.read(file).decode()
-                    == f'"{file.removesuffix(".json")}"'
-                )
 
 
 def test_download_some() -> None:
