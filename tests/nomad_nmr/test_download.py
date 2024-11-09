676ab270142c39c787f4cf7c203db41021153764
Lukas Turcani <lukasturcani93@gmail.com>
2024-11-09 21:58:12 +0000

    wip

--- tests/nomad_nmr/test_download.py
+++ tests/nomad_nmr/test_download.py
@@ -20,9 +20,6 @@ def test_download_all() -> None:
         .unique()
         .sort("spectrum")
     )
-    pl.Config.set_fmt_str_lengths(1000)
-    pl.Config.set_tbl_cols(-1)
-    pl.Config.set_tbl_rows(-1)
     expected = pl.DataFrame(
         {
             "spectrum": [
@@ -42,28 +39,23 @@ def test_download_some() -> None:
         username="admin",
         password="foo",  # noqa: S106
     )
-
     experiments = client.auto_experiments(
         nomad_nmr.AutoExperimentQuery(
             solvent="CDCl3", title=["Test Exp 1", "Test Exp 6"]
         )
     )
-    with tempfile.NamedTemporaryFile("wb") as f:
-        f.write(experiments.download())
-        f.seek(0)
-
-        expected_files = sorted(
-            [
-                "2106231050-2-1-test1-10.json",
-                "2106231050-2-1-test1-11.json",
-                "2106241100-10-2-test3-10.json",
+    spectra = (
+        bruker.nmr_peaks_df_1d(experiments.download())
+        .select("spectrum")
+        .unique()
+        .sort("spectrum")
+    )
+    expected = pl.DataFrame(
+        {
+            "spectrum": [
+                "2409231309-0-2-lukasturcani/10/pdata/1",
+                "2410161546-0-1-admin/10/pdata/1",
             ]
-        )
-        with zipfile.ZipFile(f.name) as zip_file:
-            files = sorted(zip_file.namelist())
-            assert files == expected_files
-            for file in files:
-                assert (
-                    zip_file.read(file).decode()
-                    == f'"{file.removesuffix(".json")}"'
-                )
+        }
+    ).sort("spectrum")
+    assert spectra.equals(expected)
